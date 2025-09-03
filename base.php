8875e01890c28a33ecb47fc7eb62118de8637ac2
Nguyen Minh Hieu <lotous12a4@gmail.com>
2025-09-04 05:18:23 +0700

    Update

---
+++ base.php
@@ -0,0 +1,10 @@
+<!-- Embed Favicon -->
+<link
+    href="<?php echo JURI::base(true).'/images/icons/favicon.png'; ?><?php echo C_CACHED; ?>"
+    rel="shortcut icon"
+    type="image/x-icon"
+/>
+
+<?php /* Embed fontawesome */ ?>
+<link rel="stylesheet" href="/../../../templates/fontawesome-free-6.2.0-web/css/all.min.css" />
+
